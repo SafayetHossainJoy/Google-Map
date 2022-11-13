9bdfcaf2a5d8098a3c635be8676c2ea4f9b80a71
SafayetHossainJoy <safayethossain.cse@gmail.com>
2022-11-13 22:14:32 +0600

    GoogleMap

--- googlemap/lib/map.dart
+++ googlemap/lib/map.dart
@@ -14,20 +14,55 @@ class _mapState extends State<map> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(23.710400, 90.407440),
     zoom: 14.4746,
   );
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(23.710400, 90.407440),
+        infoWindow: InfoWindow(title: 'My current Location')),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(23.819318, 90.426981),
+        infoWindow: InfoWindow(title: 'E-5 road')),
+    Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(23.859464, 90.40222),
+        infoWindow: InfoWindow(title: 'F-4 road')),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-          mapType: MapType.normal,
-          compassEnabled: false,
-          myLocationEnabled: true,
-          initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          }),
+      body: SafeArea(
+        child: GoogleMap(
+            mapType: MapType.normal,
+            compassEnabled: false,
+            myLocationEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            markers: Set<Marker>.of(_marker),
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_disabled_outlined),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(target: LatLng(23.867649, 90.393958), zoom: 14)));
+          setState(() {});
+        },
+      ),
     );
   }
 }
